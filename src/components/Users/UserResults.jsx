fe1755ecdd778c51ec3c5d375c1676165b2d5f8d
ImKetan1610 <ketan.makode@gmail.com>
2022-12-09 20:20:37 +0530

    added spinner component

--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -32,7 +33,7 @@ function UserResults() {
       </div>
     );
   }else {
-    return <h3>Loading...</h3>
+    return <Spinner />
   }
 }
 
