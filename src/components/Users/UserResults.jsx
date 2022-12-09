8d40ae20e9bbda2202510eac3a0f6b3b52ca449d
ImKetan1610 <ketan.makode@gmail.com>
2022-12-09 21:33:20 +0530

    fetched users and show it on UI with the help of useState and useEffect

--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -28,7 +29,7 @@ function UserResults() {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
