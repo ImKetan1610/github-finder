4a6285734b1657e3dbc1b3417958fd75ce0003b9
ImKetan1610 <ketan.makode@gmail.com>
2022-12-09 22:39:35 +0530

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import UserResults from "../components/Users/UserResults";
+import UserSearch from "../components/Users/UserSearch";
 
 
 function Home() {
   return <>
   {/* serch component */}
+  <UserSearch />
     <UserResults />
   </>;
 }
