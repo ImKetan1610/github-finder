7cbddaccb58ebdd0875899543b65f84605435b54
ImKetan1610 <ketan.makode@gmail.com>
2023-01-05 02:48:33 +0530

    updated single user profile

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
-import GithubContext from "../context/github/GithubContext";
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
+import GithubContext from "../context/github/GithubContext";
 
 function User() {
   const { getUser, user, loading } = useContext(GithubContext);
@@ -48,6 +48,34 @@ function User() {
               <figure>
                 <img src={avatar_url} alt="Profile Pic" />
               </figure>
+
+              <div className="card-body justify-end">
+                <h2 className="card-title mb-0">{name}</h2>
+                <p>{login}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="cols-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge badge-success">{type}</div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a
+                  href={html_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-outline"
+                >
+                  Visit Github Profile
+                </a>
+              </div>
             </div>
           </div>
         </div>
